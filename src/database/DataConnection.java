ba67d478cfe4c26be17919d54d52f834927d9ad9
Arnau <a24arnpergan@inspedralbes.cat>
2025-04-24 10:32:01 +0200

    Creació de la connexió per a la base de dades, falta posar la URL per a poder completar la connexió

---
+++ src/database/DataConnection.java
@@ -0,0 +1,19 @@
+package src.database;
+
+public class DataConnection {
+    private static String USR = "a24arnpergan_PJgrup5";
+    private static String PWD = "Password@1";
+    private static String URL = "";
+
+    public static String getUSR() {
+        return USR;
+    }
+
+    public static String getPWD() {
+        return PWD;
+    }
+
+    public static String getURL() {
+        return URL;
+    }
+}
