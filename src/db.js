7de73012f61c920b162fe761fda90aaac83ce5e3
PauGit2134 <a24pauucllah@inspedralbes.cat>
2025-05-05 11:57:02 +0200

    Arreglat el db

--- src/db.js
+++ src/db.js
@@ -1,14 +1,15 @@
+// src/db.js
 const { Sequelize } = require('sequelize');
+require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,
-  dialect: 'mysql',
-  port: 3306,
-  dialectOptions: {
-    connectTimeout: 60000, // 60 segundos
-  },
-});
+const sequelize = new Sequelize(
+  process.env.MYSQL_DATABASE,
+  process.env.MYSQL_USER,
+  process.env.MYSQL_PASSWORD,
+  {
+    host: process.env.MYSQL_HOST,
+    dialect: 'mysql',
+  }
+);
 
-sequelize.authenticate()
-  .then(() => console.log('Conexión a la base de datos exitosa'))
-  .catch(err => console.error('Error al conectar a la base de datos:', err));
+module.exports = sequelize;
